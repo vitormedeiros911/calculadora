fdcd8bf2d3f5568328b7eff369229843e4c83617
vitormedeiros911 <vitormedeiros911@outlook.com>
2020-07-18 17:21:35 -0300

    Commit inicial

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Button from './src/components/Button'
+import Display from './src/components/Display'
+
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
+
+export default class App extends React.Component {
+
+  state = { ...initialState }
+
+  handleAddDigit = n => {
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+
+    if (n === "." && !clearDisplay
+    && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
+
+  }
+
+  handleSetOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
+  }
+
+  clearMemory = () => {
+    this.setState({ ...initialState })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue} />
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={this.handleSetOperation} />
+          <Button label="7" onClick={this.handleAddDigit} />
+          <Button label='8' onClick={this.handleAddDigit} />
+          <Button label='9' onClick={this.handleAddDigit} />
+          <Button label='*' operation onClick={this.handleSetOperation} />
+          <Button label='4' onClick={this.handleAddDigit} />
+          <Button label='5' onClick={this.handleAddDigit} />
+          <Button label='6' onClick={this.handleAddDigit} />
+          <Button label='-' operation onClick={this.handleSetOperation} />
+          <Button label='1' onClick={this.handleAddDigit} />
+          <Button label='2' onClick={this.handleAddDigit} />
+          <Button label='3' onClick={this.handleAddDigit} />
+          <Button label='+' operation onClick={this.handleSetOperation} />
+          <Button label='0' double onClick={this.handleAddDigit} />
+          <Button label='.' onClick={this.handleAddDigit} />
+          <Button label='=' operation onClick={this.handleSetOperation} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+})
